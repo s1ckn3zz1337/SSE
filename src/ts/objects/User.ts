bd3b5150ca0eeacfe35ab7c70db12151fefd8df4
s1ckn3zz <alexander.kiri@hotmail.de>
2017-12-27 22:58:57 +0100

    add sha512 encryption of user password

--- src/ts/objects/User.ts
+++ src/ts/objects/User.ts
@@ -1,13 +1,16 @@
+import {saltHashPassword} from '../services/cryptoService';
+import {stringify} from "querystring";
 export class User {
     constructor(public id: string, public username: string, public password: string) {
+        this.id = id;
+        this.username = username;
+        this.password = saltHashPassword(password, username)
     }
 
     register() {
-
     }
 
     login() {
-        // do some logic here, aka prepare data
     }
 
     checkCredentials() {
