dda24a1a40b808e16e4cf7f585b3fae3e27d77b4
s1ckn3zz <alexander.kiri@hotmail.de>
2017-12-28 00:12:31 +0100

    add registration route

--- src/ts/objects/User.ts
+++ src/ts/objects/User.ts
@@ -2,15 +2,17 @@ import {saltHashPassword} from '../services/cryptoService';
 import {stringify} from "querystring";
 export class User {
     constructor(public id: string, public username: string, public password: string) {
-        this.id = id;
-        this.username = username;
         this.password = saltHashPassword(password, username)
     }
 
     register() {
+        return new Promise((resolve, reject)=>{
+            // todo add registration flow here
+        });
     }
 
     login() {
+
     }
 
     checkCredentials() {
