43b37888ac4e17a5192da842c8a05652fa5bf19e
Drakezul <sebastian.grueb@web.de>
2017-12-29 19:28:03 +0100

    sync interfaces and schemes

--- src/ts/database/schemes.ts
+++ src/ts/database/schemes.ts
@@ -1,19 +1,16 @@
-import { Schema, model, SchemaDefinition, Document } from "mongoose";
+import {Schema, model, SchemaDefinition, Document} from "mongoose";
 import * as Models from "../objects/Model";
 
-/** interfaces */
-export interface UserDocument extends Document {
-    _id: string,
-    username: string,
-    email: String,
-    password: string,
-    keyrings: KeyRingDocument[]
-}
+/** TODO sync schemas and interfaces */
 
-export interface KeyRingDocument extends Document {
-    schemaId: Schema.Types.ObjectId,
-    keyEntites: KeyEntityDocument[]
-}
+/** Key entity */
+export const keyEntitySchema: Schema = new Schema({
+    _id: Schema.Types.ObjectId,
+    keyName: String,
+    keyEncryptedPassword: String,
+    keyDescription: String,
+    keyURL: String
+});
 
 export interface KeyEntityDocument extends Document {
     schemaId: Schema.Types.ObjectId,
@@ -23,28 +20,32 @@ export interface KeyEntityDocument extends Document {
     keyURL: String
 }
 
-/** TODO sync schemas and interfaces */
-
-
-export const keyEntitySchema: Schema = new Schema({
-    _id: Schema.Types.ObjectId,
-    keyName: String,
-    keyEncryptedPassword: String,
-    keyDescription: String,
-    keyURL: String
-});
-
+/** Key Ring */
 export const keyRingSchema: Schema = new Schema({
     _id: Schema.Types.ObjectId,
     keyEntites: [keyEntitySchema]
 });
+export interface KeyRingDocument extends Document {
+    schemaId: Schema.Types.ObjectId,
+    keyEntites: KeyEntityDocument[]
+}
 
+/** User */
 export const userSchema: Schema = new Schema({
     _id: Schema.Types.ObjectId,
     username: String,
     password: String,
+    email: String,
     keyrings: [keyRingSchema]
 });
+
+export interface UserDocument extends Document {
+    _id: string,
+    username: string,
+    email: string,
+    password: string,
+    keyrings: KeyRingDocument[]
+}
 
 export const KeyEntity = model<KeyEntityDocument>('KeyEntity', keyEntitySchema);
 export const KeyRing = model<KeyRingDocument>('KeyRing', keyRingSchema);
