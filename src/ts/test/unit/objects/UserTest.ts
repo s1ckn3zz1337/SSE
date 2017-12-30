30bcb507ba7949f2a6fd2683c2a7df9930534853
s1ckn3zz <alexander.kiri@hotmail.de>
2017-12-30 19:38:59 +0100

    fix tests

--- src/ts/test/unit/objects/UserTest.ts
+++ src/ts/test/unit/objects/UserTest.ts
@@ -8,7 +8,7 @@ const DEFAULT_USERNAME = 'username';
 const DEFAULT_PASSWORD = 'password';
 describe('Test User Class', () => {
     it('Should hash the clear text password on object creation', () => {
-        const newUser = new User("null", DEFAULT_USERNAME, DEFAULT_PASSWORD, "email", []);
+        const newUser = new User("null", DEFAULT_USERNAME, DEFAULT_PASSWORD, "email", [], false);
         const pwdHash = crypto.saltHashPassword(DEFAULT_PASSWORD, DEFAULT_USERNAME);
         expect(pwdHash).to.equal(newUser.password);
     });
