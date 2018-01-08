431ba3b44df34fe37e723bd8702d00637911f969
Philipp Minges <fjnalta@gmail.com>
2018-01-08 18:59:40 +0100

    fix config fail

--- src/ts/config/config.ts
+++ src/ts/config/config.ts
@@ -4,7 +4,7 @@ export class Env {
     public static readonly webContentDir = '/static';
     public static readonly port = 443;
     public static readonly mongoDB: MongoConfig = {
-        host: '0.0.0.0',
+        host: 'localhost',
         port: 27017,
         user: 'sseuser',
         password: 'Z2xQK8tASSDF8ZcU3XuSLzNY',
