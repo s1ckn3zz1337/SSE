2d07e79e49305c6afb707c8ab5c6744521405c9f
s1ckn3zz <alexander.kiri@hotmail.de>
2018-01-09 12:45:13 +0100

    add docker start script for the DB

---
+++ deployment/startFreshDBDocker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker stop sse-database
+docker rm sse-database
+rm -r ./database/sse_mongodb
+mkdir ./database/sse_mongodb
+docker run -p 27017:27017 --name sse-database -v $PWD/database/sse_mongodb:/data/db -d mongo --auth
+docker exec -i sse-database mongo < $PWD/database/mongodbConfig.js
