a0f413a67e899626728e3876a72b977451260eb0
Drakezul <sebastian.grueb@web.de>
2018-01-09 10:05:51 +0100

    Slight improvement to createSSEUser

--- doc/MongoSetup/Windows/2.createSSEUser.bat
+++ doc/MongoSetup/Windows/2.createSSEUser.bat
@@ -2,8 +2,8 @@
 REM set the flag below to true, if you changed the password for sseUser
 SET updatedJavascriptFiles=
 if "%updatedJavascriptFiles%" == "" (
-    echo.Please set the password of the sseUser in createSSEUser.js, then set the flag in this script file to true
-    echo.After running this script you might want to reset the password in the file, so that it does not remain readable
+    echo.Please update the passwords used in createSSEUser.js, then set the flag in this script file to true
+    echo.After running this script you might want to reset the password in the createUser.js, so that it does not remain readable
     pause >nul
     goto :eof
 )
