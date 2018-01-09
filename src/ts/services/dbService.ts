c1b9b787a616fef43d7c98d08111fa906819750c
Drakezul <sebastian.grueb@web.de>
2018-01-09 09:43:31 +0100

    Added "input validation"

--- src/ts/services/dbService.ts
+++ src/ts/services/dbService.ts
@@ -76,6 +76,9 @@ export function registerUser(data: User): Promise<User> {
 };
 
 export function findUser(userName: string): Promise<User[]> {
+    if(userName == "" || userName == undefined || userName == null){
+        return new Promise<User[]>((a,b)=>{a([])});
+    }
     return scheme.User.find({ $where: "this.username.includes("+userName+")"})
     .then(res => {
         return User.getFromDocuments(res);
