7af6b0cc7042e5612675ca97d601646f5905cfcf
s1ckn3zz <alexander.kiri@hotmail.de>
2018-01-16 00:24:25 +0100

    improve config

--- src/ts/config/config.ts
+++ src/ts/config/config.ts
@@ -1,11 +1,10 @@
-import * as mongoDB from "mongoose";
-
 export class Env {
     public static readonly webContentDir = '/static';
     public static readonly port = 443;
     // hange pepper on deployment
     public static readonly pepper = 'CHANGE_ME_ON_DEPLYOMENT!!!';
     public static readonly mongoDB: MongoConfig = {
+        // https://linuxconfig.org/basic-example-on-how-to-link-docker-containers
         host: 'mongo',
         port: 27017,
         user: 'sseuser',
